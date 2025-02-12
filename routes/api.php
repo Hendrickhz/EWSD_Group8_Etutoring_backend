20e6834557c3a7634e24efadda9c7e8c8fddcf8c
ヘンドリック Hendrick(Hein Htet Zaw) <hhzaw325@gmail.com>
2025-02-12 21:19:37 +0630

    added auth check to student allocation, added staff only middleware to student allocation routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AllocationController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MeetingController;
+use App\Http\Middleware\StaffOnly;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,15 +12,16 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-// Allocation, Reallocation
-Route::controller(AllocationController::class)->group(function () {
-    Route::post('/allocate-student', 'allocateStudent');
-    Route::post('/bulk-allocate', 'bulkAllocate');
-    Route::get('/tutor/{id}/students', 'getTutorStudents');
-    Route::delete('/remove-tutor', 'removeTutorFromStudent');
+Route::middleware('auth:sanctum')->group(function () {
+    // Allocation, Reallocation
+    Route::controller(AllocationController::class)->middleware(StaffOnly::class)->group(function () {
+        Route::post('/allocate-student', 'allocateStudent');
+        Route::post('/bulk-allocate', 'bulkAllocate');
+        Route::get('/tutor/{id}/students', 'getTutorStudents');
+        Route::delete('/remove-tutor', 'removeTutorFromStudent');
+    });
 });
 
-
-Route::post('/auth/login',[AuthController::class, 'LoginUser']);
-
+// Authentication
+Route::post('/auth/login', [AuthController::class, 'LoginUser']);
 Route::middleware('auth:sanctum')->post('/auth/logout', [AuthController::class, 'logoutUser']);
