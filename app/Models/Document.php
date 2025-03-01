68c4f8307291705900af1ecc26e139c1baf960ba
ヘンドリック Hendrick(Hein Htet Zaw) <hhzaw325@gmail.com>
2025-03-01 14:54:52 +0630

    fixed url

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -25,6 +25,6 @@ class Document extends Model
 
     public function getFullUrlAttribute()
     {
-        return 'http:127.0.0.1:8000'.Storage::url($this->path);
+        return asset("storage/{$this->path}");
     }
 }
