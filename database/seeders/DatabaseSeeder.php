4dcc52b37893c71d7f7eb321131cf24f6675c5f0
ヘンドリック Hendrick(Hein Htet Zaw) <hhzaw325@gmail.com>
2025-03-04 19:59:11 +0630

    added seeding logic for user's last_login column

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +20,12 @@ class DatabaseSeeder extends Seeder
             DocumentSeeder::class,
             MessageSeeder::class
         ]);
+
+        User::all()->each(function ($user){
+            if($user->last_active_at){
+                $oneHourBefore = Carbon::parse($user->last_active_at)->subHour();
+                $user->update(['last_login' => $oneHourBefore]);
+            }
+        });
     }
 }
